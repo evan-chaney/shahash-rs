7bcee29c58a67d2bb3d546b23ecf4052eae271d7
Evan Chaney <evanachaney@gmail.com>
2020-10-11 20:11:04 -0500

    Spin up my state array

--- src/main.rs
+++ src/main.rs
@@ -37,29 +37,41 @@ fn pad(n: &mut BitVec, rate: u16) -> &BitVec {
 }
 
 // Permutation/state transformation function
-fn permutate(A: BitVec, block_width: u8, rate: u16, output_length: u16) {
+fn permutate(A: &BitVec, word_size: u16, rate: u16, output_length: u16) {
     // Endian here is little-endian
     // State is a 5 x 5 x W (row, column, bit) array
+    let mut state: Vec<Vec<BitVec>> = Vec::with_capacity(5);
+    let mut memoffset: usize = 0;
+    // Do some wild seeding of the state array
+    for row in 0..4 {
+        state.insert(row, Vec::with_capacity(5));
+        for col in 0..4 {
+            state[row].insert(col, BitVec::with_capacity(word_size as usize));
 
+            state[row][col] = A[memoffset..memoffset + word_size as usize].to_bitvec();
+            memoffset += word_size as usize;
+        }
+    }
     // Initialize some intermediate variables
 
-    let b: Vec<BitVec> = Vec::with_capacity(5);
-    let c: Vec<BitVec> = Vec::with_capacity(5);
-    let d: Vec<BitVec> = Vec::with_capacity(5);
+    let mut b: Vec<BitVec> = Vec::with_capacity(5);
+    let mut c: Vec<BitVec> = Vec::with_capacity(5);
+    let mut d: Vec<BitVec> = Vec::with_capacity(5);
     //let mut b: BitVec<LocalBits, usize> = BitVec::with_capacity(64); // is 64 the right number here???
     //let mut c: BitVec<LocalBits, usize> = BitVec::with_capacity(64);
     //let mut d: BitVec<LocalBits, usize> = BitVec::with_capacity(64);
 
     for x in 0..4 {
-        c[x] = A[x][0]
+        //c.insert(x, state[x][0] ^ state[x][1]);
     }
 }
 
 fn main() {
     // N is our input bit string
     let mut n = bitvec![1, 1, 0, 1, 0, 0, 1, 1];
+    let mut padded_array = pad(&mut n, R);
+    permutate(padded_array, W, R, D);
 
-    println!("Length: {}", n.len());
-    println!("Length: {}", pad(&mut n, R).len());
+    //   println!("Test bitvec 32: {:?}", bitvec![32]);
     //    println!("AND: {}", &n & bitvec![0, 1, 1, 0]);
 }
